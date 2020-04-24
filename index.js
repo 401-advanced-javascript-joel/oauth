62a176406162a582d9f43a109de134b8d0759052
Joel Watson <Joel.watson92@gmail.com>
2020-04-24 02:34:30 -0700

    Updating comments, remoteApiUrls, & response handling

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 'use strict';
 
 // TODO: What is this line doing?
+// DONE: This line is setting all the environement variables
 require('dotenv').config();
 
 // TODO: What is this line doing?
+// DONE: This line is spinning up the server on the given port
 require('./lib/server.js').start(process.env.PORT);
